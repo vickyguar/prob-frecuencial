045d084f94bd9335efccd2d2079f9fc9a39e2153
vickyguar <vickyguarnieri1@gmail.com>
2026-01-13 13:20:54 -0300

    mejoras

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ ___
 
 ## Cuál es la probabilidad de ganar el envido teniendo 28?
 
-<img width="200" alt="image" src="https://github.com/user-attachments/assets/be23f43b-81af-4b8d-9779-790c711f6e90" />
+<img alt="image" src="./src/milito.png" />
 
-En la [notebook](https://github.com/vickyguar/prob-frecuencial/blob/main/simulacion.ipynb) se realiza una simulación que estima que la probabilidad de ganar el envido con 28 es aproximadamente 0,84. Hay un pico en el histograma de frecuencias en 7 (ojo ahora en 6, revisar!), que se da porque en cada mano estoy obligando a elegir el valor más alto. Esto no quiere decir que sea el valor que se cante en el envido, porque se puede expresar "mesa" para decir que no se tiene ni 20 en caso de que no hayan 2 cartas del mismo palo, por ejemplo. Esto está simulado para jugar al envido en casos de que se juegue 1 vs 1, y está pendiente la simulación para el envido en equipos para el truco de a 4 o 6.
+En la [notebook](https://github.com/vickyguar/prob-frecuencial/blob/main/simulacion.ipynb) se realiza una simulación que estima que la probabilidad de ganar el envido con 28 es aproximadamente 0,86. Hay un pico en el histograma de frecuencias en 7, que se da porque en cada mano estoy obligando a elegir el valor más alto. Esto no quiere decir que sea el valor que se cante en el envido, porque uno puede expresar "mesa" para decir que no se tiene ni 20 en caso de que no hayan 2 cartas del mismo palo, por ejemplo. Esto está simulado para jugar al envido en casos de que se juegue 1 vs 1.
 
 ___
-## Probabilidad de tener al menos dos cartas del mismo palo
+## Probabilidad de tener envido
 
-Por otro lado, si calculamos las probabilidades de que hayan dos cartas del mismo palo (y llegar a los 20 puntos) tomando tres cartas, podemos hacer esta cuenta:
+Por otro lado, si calculamos las probabilidades de que hayan dos cartas del mismo palo (y llegar al menos a los 20 puntos) tomando tres cartas, podemos hacer esta cuenta:
 
 **Total de combinaciones posibles de 3 cartas**:
 
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 from src.constants import TODAS_CARTAS
 import random
+import pandas as pd
 from collections import defaultdict
 
-random.seed(42)  # Reproducibilidad
+random.seed(42)
 
 def repartir_mano(cartas: list = TODAS_CARTAS, numero_cartas: int = 3, mis_cartas = None) -> list:
     if mis_cartas:
@@ -35,9 +36,9 @@ def calcular_envido(mano: list) -> int:
     return mejor
 
 
-
 def simular_envidos_list(mis_cartas: list, numero_simulaciones: int = 10000) -> list:
     envidos_oponente = []
+    
     for _ in range(numero_simulaciones):
         mano_oponente = repartir_mano(TODAS_CARTAS, 3, mis_cartas)
         envido_oponente = calcular_envido(mano_oponente)
@@ -45,15 +46,48 @@ def simular_envidos_list(mis_cartas: list, numero_simulaciones: int = 10000) -> 
     return envidos_oponente
 
 
-def simular_envido(mis_cartas: list, numero_simulaciones: int = 10000) -> float:
-    victorias = 0
-    mi_envido = calcular_envido(mis_cartas)
-    for _ in range(numero_simulaciones):
-        mano_oponente = repartir_mano(mis_cartas, TODAS_CARTAS)
+def simulacion_completa(numero_simulaciones: int = 10000) -> pd.DataFrame:
+    envidos_oponente = []
+    cartas_oponente = []
+    mis_envidos = []
+    mis_cartas = []
+    victorias = []
+    soy_manos = []
+    
+    for i in range(numero_simulaciones):
+        
+        soy_mano = (i % 2 == 0) # par, por ejemplo
+
+        if soy_mano:
+            mi_mano = repartir_mano(TODAS_CARTAS, 3)
+            mano_oponente = repartir_mano(TODAS_CARTAS, 3, mi_mano)
+        else:
+            mano_oponente = repartir_mano(TODAS_CARTAS, 3)
+            mi_mano = repartir_mano(TODAS_CARTAS, 3, mano_oponente)
+
+        mi_envido = calcular_envido(mi_mano)
         envido_oponente = calcular_envido(mano_oponente)
+
         if mi_envido > envido_oponente:
-            victorias += 1
-    probabilidad_ganar = victorias / numero_simulaciones
-    return probabilidad_ganar
-
-
+            victoria = True
+        elif mi_envido < envido_oponente:
+            victoria = False
+        else:
+            victoria = soy_mano  # empate lo gana mano
+        
+        # acumulo
+        envidos_oponente.append(envido_oponente)
+        cartas_oponente.append(mano_oponente)
+        mis_envidos.append(mi_envido)
+        mis_cartas.append(mi_mano)
+        soy_manos.append(soy_mano)
+        victorias.append(victoria)
+    
+    return pd.DataFrame({
+        "envido_oponente": envidos_oponente,
+        "mano_oponente": cartas_oponente,
+        "mi_envido": mis_envidos,
+        "mi_mano": mis_cartas,
+        "soy_mano": soy_manos,
+        "victoria": victorias
+    })
